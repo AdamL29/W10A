6e16c1c82c7e435b854a33b9ae48c76411475762
Adam <adamlederer@live.ca>
2022-11-08 22:18:06 -0700

    finished function 1. Started function 2 & 3.

---
+++ js/apps.js
@@ -0,0 +1,20 @@
+function sum(num1, num2) {
+    let result = (num1 + num2) * 5;
+    return result;
+}
+console.log(sum(4,8));
+
+function users(user1, user2, user3, user4, user5, user6, user7, user8, use9, user10, user11) {
+    if (users > 10) {
+        return `true`;
+    } else {
+        return `false`;
+    }
+}
+
+
+
+function element() {
+    console.log();
+    
+}
